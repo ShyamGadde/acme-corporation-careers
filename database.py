7309949a0939039cd9c23c4450c39cefb812b009
Shyamsundar Gadde <73636812+ShyamGadde@users.noreply.github.com>
2023-02-23 16:44:01 +0530

    Add database.py

---
+++ database.py
@@ -0,0 +1,4 @@
+from sqlalchemy import create_engine
+
+# sqlalchemy engine connect to mysql
+engine = create_engine('mysql+pymysql://salkfo8d9lyat3udfss5:pscale_pw_tqBXiUF11z9Zd1Kx18FYHdQqJtKVTy5yfrSkZahYIa8@ap-south.connect.psdb.cloud/acme-corporation-careers')
