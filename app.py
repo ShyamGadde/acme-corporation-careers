55f51de460f32a78bb608c7404fd1ebd5833ebe6
Shyamsundar Gadde <73636812+ShyamGadde@users.noreply.github.com>
2023-02-23 20:02:56 +0530

    Add custom 404 page

--- app.py
+++ app.py
@@ -19,4 +19,6 @@ def api_jobs():
 
 @app.route('/job/<int:job_id>')
 def apply(job_id):
-    return render_template("apply.html", job_details=get_job_details(job_id))
+    if job_details := get_job_details(job_id):
+        return render_template("apply.html", job_details=job_details)
+    return render_template("404.html")
