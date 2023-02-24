6e89ccd18301281125a7e8aeae61fdb2e71b875f
Shyamsundar Gadde <73636812+ShyamGadde@users.noreply.github.com>
2023-02-24 12:26:35 +0530

    Add function to upload the application to database

--- database.py
+++ database.py
@@ -23,3 +23,19 @@ def get_job_details(job_id):
         return conn.execute(
             text("SELECT * FROM jobs WHERE id = :job_id"), {"job_id": job_id}
         ).first()
+
+
+def upload_application(job_id, application):
+    with engine.connect() as conn:
+        conn.execute(
+            text(
+                "INSERT INTO applications (job_id, full_name, email, phone, resume_url) VALUES (:job_id, :name, :email, :phone, :resume)"
+            ),
+            {
+                "job_id": job_id,
+                "name": application["name"],
+                "email": application["email"],
+                "phone": application["phone"],
+                "resume": application["resume"],
+            },
+        )
